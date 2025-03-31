64bb0b188da346ee41feebb1498fba6247c10b5f
keislypasoss <pkeisly60886@ufide.ac.cr>
2025-03-30 23:08:32 -0600

    UpdateService

---
+++ src/main/java/com/Panaderia/service/ReposteriaService.java
@@ -0,0 +1,16 @@
+package com.Panaderia.service;
+
+import com.Panaderia.domain.Reposteria;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReposteriaService {
+  List<Reposteria> findAll();
+
+  Optional<Reposteria> findById(Integer id);
+
+  Reposteria save(Reposteria reposteria);
+
+  void deleteById(Integer id);
+}
+
