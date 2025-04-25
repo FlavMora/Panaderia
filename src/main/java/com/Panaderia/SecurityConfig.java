d1795f983ad67d5037cdfecc537afc5f45b190b5
Esteban Ruiz <wirhoss@gmail.com>
2025-04-25 00:28:58 +0000

    hotfix

--- src/main/java/com/Panaderia/SecurityConfig.java
+++ src/main/java/com/Panaderia/SecurityConfig.java
@@ -9,71 +9,76 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.DispatcherType;
+
 import org.springframework.beans.factory.annotation.Autowired; // Import Autowired
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public UserDetailsService users() {
-        UserDetails admin = User.builder()
-                .username("juan")
-                .password("{noop}123")
-                .roles("USER", "VENDEDOR", "ADMIN")
-                .build();
-        UserDetails sales = User.builder()
-                .username("rebeca")
-                .password("{noop}456")
-                .roles("VENDEDOR", "USER")
-                .build();
-        UserDetails user = User.builder()
-                .username("pedro")
-                .password("{noop}789")
-                .roles("USER")
-                .build();
-        UserDetails eruiz30353 = User.builder()
-                .username("eruiz30353")
-                .password("{noop}30353")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user, sales, admin, eruiz30353);
-    }
+  @Bean
+  public UserDetailsService users() {
+    UserDetails admin = User.builder()
+        .username("juan")
+        .password("{noop}123")
+        .roles("USER", "VENDEDOR", "ADMIN")
+        .build();
+    UserDetails sales = User.builder()
+        .username("rebeca")
+        .password("{noop}456")
+        .roles("VENDEDOR", "USER")
+        .build();
+    UserDetails user = User.builder()
+        .username("pedro")
+        .password("{noop}789")
+        .roles("USER")
+        .build();
+    UserDetails eruiz30353 = User.builder()
+        .username("eruiz30353")
+        .password("{noop}30353")
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(user, sales, admin, eruiz30353);
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "/",
-                                "/css/**",
-                                "/images/**",
-                                "/index",
-                                "/js/**",
-                                "/logout",
-                                "/pasteles/listado",
-                                "/postres/listado",
-                                "/reposterias/listado",
-                                "/resena/listado",
-                                "/webjars/**")
-                        .permitAll()
-                        .requestMatchers(
-                                "/admin/**",
-                                "/postres/**",
-                                "/reposterias/**",
-                                "/resena/**",
-                                "pasteles/**"
-                        ).hasRole("ADMIN")
-                        .requestMatchers("/carrito/**").hasRole("USER")
-                        .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/")
-                        .permitAll())
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout")
-                        .permitAll());
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .dispatcherTypeMatchers(DispatcherType.ERROR, DispatcherType.FORWARD).permitAll()
+            .requestMatchers(
+                "/",
+                "/css/**",
+                "/images/**",
+                "/index",
+                "/js/**",
+                "/logout",
+                "/pasteles/listado",
+                "/postres/listado",
+                "/reposterias/listado",
+                "/resena/listado",
+                "/webjars/**",
+                "/error")
+            .permitAll()
+            .requestMatchers(
+                "/admin/**",
+                "/postres/**",
+                "/reposterias/**",
+                "/resena/**",
+                "pasteles/**")
+            .hasRole("ADMIN")
+            .requestMatchers("/carrito/**").hasRole("USER")
+            .anyRequest().authenticated())
+        .formLogin(form -> form
+            .loginPage("/login")
+            .defaultSuccessUrl("/")
+            .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login?logout")
+            .permitAll());
 
-        return http.build();
-    }
+    return http.build();
+  }
 }
